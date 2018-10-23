5c6f7523f0a99800a28aae5dee8cd480c0729ee9
Syndicist <31106886+Syndicist@users.noreply.github.com>
2018-10-22 22:24:41 -0400

    COMBO GET

--- scenes/Rose_combo.gd
+++ scenes/Rose_combo.gd
@@ -30,10 +30,15 @@ var state;
 enum ATTACK{
 slash,
 pierce,
-bash
+bash,
+NIL
 }
 var first_attack;
+var second_attack;
+var third_attack;
 var combo_step;
+var interruptable;
+var timer;
 
 #initialize variables
 func _ready():
@@ -56,57 +61,96 @@ func _ready():
 	air_time = 0;
 	prev_velocity = Vector2(0,0);
 
-	### default state vars ###
+	### default stat vars ###
 	state = STATE.move_state;
 
 	### default attack vars ###
-	first_attack = ATTACK.slash;
+	first_attack = ATTACK.NIL;
+	second_attack = ATTACK.NIL;
+	third_attack = ATTACK.NIL;
 	combo_step = 0;
-
+	interruptable = false;
+	timer = 0;
 	pass
+
 
 func _physics_process(delta):
 	#count time in air
 	air_time += delta;
-
+	
 	velocity += gravity_vector * delta;
 	move_and_slide(velocity, floor_normal);
 	if(is_on_floor()):
 		air_time = 0;
 		velocity.y = 0;
-
+	
+	timer-= 1
+	
 	match(state):
 		STATE.attack_state:
 			AttackState();
 		STATE.move_state:
 			MoveState(delta);
-
+	
 	pass
 
 ### Player Attack Combos ###
 func AttackState():
+	#animation canceling
+	interruptable = false;
+	#finds what attack to do first based on input
 	match(first_attack):
 		ATTACK.slash:
-			if(anim == "StartSlash" && $StartSlashSprites.frame == 2 && combo_step == 0):
-				var effect = preload("res://scenes/StartSlashEffect.tscn").instance();
-				effect.position = Vector2(0,0);
-				effect.add_collision_exception_with(self);
-				self.add_child(effect);
-				effect.get_node("animator").play("slash");
-				combo_step += 1;
-			changeSprite("StartSlashSprites","StartSlash");
+			firstSlash();
+		ATTACK.pierce:
+			null;
+		ATTACK.bash:
+			null;
+		ATTACK.NIL:
+			null;
+	#finds what attack to do second based on input
+	match(second_attack):
+		ATTACK.slash:
+			secondSlash();
+		ATTACK.pierce:
+			null;
+		ATTACK.bash:
+			null;
+		ATTACK.NIL:
+			null;
+	#after 35 ticks, the animation can be cancled to do the next combo or reposition
+	if(timer <= 35):
+		interruptable = true;
+	else:
+		interruptable = false;
+	if(interruptable):
+		#reposition Rose, TODO: handle special cases like "jump"
+		if(Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_right") || Input.is_action_just_pressed("ui_jump")):
+			combo_step = 0;
+		#continue the combo. After so long, Rose falls off balance and is stuck finishing the animation.
+		elif(combo_step == 1 && timer > 25):
+			if(Input.is_action_just_pressed("ui_slash")):
+				second_attack = ATTACK.slash;
+				timer = 75;
+	#the combo is finished
+	if(timer <= 0):
+		combo_step = 0;
 	if(new_anim != anim):
 		Animate();
-	if(!$animator.is_playing()):
+	#THIS CAUSES EFFECT TO FLIP WHEN INTERRUPTED, NEEDS REVISION
+	if($StartSlashSprites.frame == 2 && combo_step == 0):
+		first_attack = ATTACK.NIL;
+		second_attack = ATTACK.NIL;
+		third_attack = ATTACK.NIL;
 		currentSprite.visible = false;
 		state = STATE.move_state;
-		combo_step = 0;
 	pass
 
 ### Move the player ###
 func MoveState(delta):
 	### attack triggers ###
 	if(is_on_floor() && Input.is_action_just_pressed("ui_slash")):
+		timer = 75;
 		first_attack = ATTACK.slash;
 		state = STATE.attack_state;
 		velocity.x = 0;
@@ -168,3 +212,24 @@ func changeSprite(sprite, animation):
 	new_anim = animation;
 	pass
 
+### SLASH combos ###
+func firstSlash():
+	changeSprite("StartSlashSprites","StartSlash");
+	if(anim == "StartSlash" && $StartSlashSprites.frame == 2 && combo_step == 0):
+		var effect = preload("res://scenes/StartSlashEffect.tscn").instance();
+		effect.position = Vector2(0,0);
+		effect.add_collision_exception_with(self);
+		self.add_child(effect);
+		effect.get_node("animator").play("slash");
+		combo_step += 1;
+	pass
+func secondSlash():
+	changeSprite("MidSlashSprites","MidSlash");
+	if(anim == "MidSlash" && $MidSlashSprites.frame == 2 && combo_step == 1):
+		var effect = preload("res://scenes/StartSlashEffect.tscn").instance();
+		effect.position = Vector2(0,0);
+		effect.add_collision_exception_with(self);
+		self.add_child(effect);
+		effect.get_node("animator").play("slash");
+		combo_step += 1;
+	pass
