2e3701ff490f6f374a4d594f02ab73e45aca2309
Syndicist <tylerroman95@gmail.com>
2018-10-25 00:46:05 -0400

    XXX combo 80% done

--- scenes/2nd_Slash_Effect_XXX.gd
+++ scenes/2nd_Slash_Effect_XXX.gd
@@ -5,7 +5,7 @@ var velocity = Vector2(spd,0);
 
 func _physics_process(delta):
 	var player = get_parent().get_node("Rose");
-	if(player.Direction == "left" && player.combo_step == 2):
+	if(player.Direction == "left" && player.combo_step == 3):
 		velocity.x = -spd;
 	move_and_collide(velocity*delta);
 	if(!$animator.is_playing()):
